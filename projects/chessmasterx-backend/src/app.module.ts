9391c9dd2c7db0b14839236b822c694f92dcb18d
pasindu pramodya <git@pasindupramodya.com>
2024-02-12 16:10:47 +0000

    add database connection

--- projects/chessmasterx-backend/src/app.module.ts
+++ projects/chessmasterx-backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from 'nestjs-prisma';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule.forRootAsync({
+    isGlobal: true,
+    useFactory: () => ({
+      prismaOptions: {
+        log: ['info', 'query'],
+      },
+      explicitConnect: false,
+    }),
+  }), AuthenticationModule,],
   controllers: [AppController],
   providers: [AppService],
 })
